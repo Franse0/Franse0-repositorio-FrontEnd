62d6d21afabc9d96229456a893af69f1fe9c25ed
Franse0 <panchymarin.fn@gmail.com>
2022-12-09 13:06:19 -0300

    portfolio Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExperienciaComponent,
+    EducacionComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
